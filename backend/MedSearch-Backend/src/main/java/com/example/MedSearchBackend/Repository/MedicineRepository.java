8df0e1372e4cd109afe42e14b6ea6806c61633d5
Shashini Upekha <e17356@eng.pdn.ac.lk>
2022-12-15 10:01:08 +0530

    added find all available medicine query

--- backend/MedSearch-Backend/src/main/java/com/example/MedSearchBackend/Repository/MedicineRepository.java
+++ backend/MedSearch-Backend/src/main/java/com/example/MedSearchBackend/Repository/MedicineRepository.java
@@ -18,5 +18,10 @@ public interface MedicineRepository extends JpaRepository<Medicine, MedicinePkId
     // a method to find the medicine with correct company and medicine details
     @Query(value = "SELECT * FROM medicine WHERE med_name =?1 AND comp_reg_no =?2",nativeQuery = true)
     Medicine findByNameAndCompany(String medName,String compRegNo);
+    
+    
+    //a method to find the all available medicine details
+    @Query(value = "SELECT * FROM medicine WHERE availability='YES'",nativeQuery = true)
+    List<Medicine> findByAvilability();
 
 }
