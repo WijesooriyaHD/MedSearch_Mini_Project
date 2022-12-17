e2a941846ae436bc5f5172ff2fffc7f37ab78f8c
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2022-12-17 09:32:05 +0530

    added API to get the owner by id no and pasword

--- backend/MedSearch-Backend/src/main/java/com/example/MedSearchBackend/Repository/ShopOwnerRepository.java
+++ backend/MedSearch-Backend/src/main/java/com/example/MedSearchBackend/Repository/ShopOwnerRepository.java
@@ -1,9 +1,14 @@
 package com.example.MedSearchBackend.Repository;
 
+import com.example.MedSearchBackend.Model.Medicine;
 import com.example.MedSearchBackend.Model.ShopOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ShopOwnerRepository extends JpaRepository<ShopOwner,String> {
 
+    // find a shop onwner by id no and password
+    @Query(value = "SELECT * FROM pharmacy_owner WHERE id_no =?1 AND password=?2",nativeQuery = true)
+    ShopOwner findByIdAndPassword(String idNo, String Password);
 
 }
