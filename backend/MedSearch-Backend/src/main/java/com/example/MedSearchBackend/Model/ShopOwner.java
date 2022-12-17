9b0a812563bc6630a9af119e4aa8c82b375051ab
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2022-12-17 08:57:06 +0530

    updated backend -  ceate shop owner model

---
+++ backend/MedSearch-Backend/src/main/java/com/example/MedSearchBackend/Model/ShopOwner.java
@@ -0,0 +1,46 @@
+package com.example.MedSearchBackend.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="pharmacy_owner")
+public class ShopOwner {
+    @Id
+    private String idNo;
+
+    @Column(name="passowrd")
+    private String password;
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ShopOwner() {
+    }
+
+    public ShopOwner(String idNo, String password) {
+        this.idNo = idNo;
+        this.password = password;
+    }
+
+    public ShopOwner(String password) {
+        this.password = password;
+    }
+
+
+}
