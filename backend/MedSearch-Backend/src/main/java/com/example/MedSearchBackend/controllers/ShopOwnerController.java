8d150a0967bbd2da7f671c60db49d9ef2757fe38
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2022-12-17 09:28:06 +0530

    added shopOwnerController

---
+++ backend/MedSearch-Backend/src/main/java/com/example/MedSearchBackend/controllers/ShopOwnerController.java
@@ -0,0 +1,32 @@
+package com.example.MedSearchBackend.controllers;
+
+import com.example.MedSearchBackend.Model.Medicine;
+import com.example.MedSearchBackend.Model.ShopOwner;
+import com.example.MedSearchBackend.Repository.MedicineRepository;
+import com.example.MedSearchBackend.Repository.ShopOwnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/")
+public class ShopOwnerController {
+
+    @Autowired
+
+    private ShopOwnerRepository shopOwnerRepository;
+
+    //get all shop owners
+    @GetMapping("/shopowners")
+    public List<ShopOwner> getallShopOwners(){
+
+        return shopOwnerRepository.findAll();
+    }
+
+    // get shopOwner by id and password
+
+
+}
