43e7a4af8925920c8361cfbb621e70d222a48bf9
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2022-12-17 09:03:20 +0530

    added shopOwnerRepository

---
+++ backend/MedSearch-Backend/src/main/java/com/example/MedSearchBackend/Repository/ShopOwnerRepository.java
@@ -0,0 +1,9 @@
+package com.example.MedSearchBackend.Repository;
+
+import com.example.MedSearchBackend.Model.ShopOwner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopOwnerRepository extends JpaRepository<ShopOwner,String> {
+
+
+}
