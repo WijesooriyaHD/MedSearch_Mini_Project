c899e26f33985d57a1702b37fad9b3813396aea1
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2023-01-12 10:02:43 +0530

    new method added

--- backend/MedSearch-Backend/src/main/java/com/example/MedSearchBackend/MedSearchBackendApplication.java
+++ backend/MedSearch-Backend/src/main/java/com/example/MedSearchBackend/MedSearchBackendApplication.java
@@ -2,9 +2,18 @@ package com.example.MedSearchBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class MedSearchBackendApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return ("backend has been successfully deployed in azure ");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedSearchBackendApplication.class, args);
